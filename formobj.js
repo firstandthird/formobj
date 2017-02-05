821c7595e4b187cb99d32e8d36fed04e9cd464db
Antonio Laguna <alaguna@google.com>
2017-02-05 23:46:15 +0100

    Updating formObj

--- formobj.js
+++ formobj.js
@@ -1,24 +1,31 @@
 import deserialize from './lib/deserialize';
 import getInputs from './lib/getInputs';
 import getJSON from './lib/getJSON';
+import getQueryString from './lib/getQueryString';
 import isForm from './lib/isForm';
 
 function formobj(form, selector) {
   isForm(form);
 
-  return {
+  const api = {
     getInputs() {
       return getInputs(form, selector);
     },
 
     getJSON() {
-      return getJSON(getInputs(form, selector));
+      return getJSON(form, selector);
     },
 
     deserialize(data) {
       return deserialize(data, getInputs(form, selector));
+    },
+
+    getQueryString() {
+      return getQueryString(api.getJSON(form));
     }
   };
+
+  return api;
 }
 
-export { formobj as default, deserialize, getInputs, getJSON, isForm };
+export { formobj as default, deserialize, getInputs, getJSON, isForm, getQueryString };
--- test/formobj.test.js
+++ test/formobj.test.js
@@ -1,24 +1,81 @@
 /* eslint no-consoSerializeFormle: 0 */
 
-import formObj from '../formobj';
-
-const form1 = formObj(document.getElementById('exampleform'));
-const form2 = formObj(document.getElementById('exampleform2'));
+import { default as formObj, getQueryString } from '../formobj';
 
 import test from 'tape-rollup';
 
+let form1;
+let form2;
+
+const init = () => {
+  const container = document.createElement('div');
+  container.id = 'formobj';
+  document.body.appendChild(container);
+};
+
+const setup = () => {
+  const container = document.getElementById('formobj');
+  container.innerHTML = `
+    <form id="exampleform">
+      <input type="text" name="test1" value="test">
+      <input type="text" value="nope">
+      <input type="text" name="arraylike" value="val1">
+      <input type="text" name="arraylike" value="val2">
+      <input type="checkbox" name="checkbox" checked>
+      <input type="checkbox" name="checkbox">
+      <input type="radio" name="gender" value="male">
+      <input type="radio" name="gender" value="female" checked>
+      <input type="radio" name="gender" value="other">
+      <select name="color">
+        <option value="red">Red</option>
+        <option value="green" selected>Green</option>
+        <option value="blue">Blue</option>
+      </select>
+      <select name="sizes" multiple>
+        <option value="small" selected>Small</option>
+        <option value="medium" selected>Medium</option>
+        <option value="large">Large</option>
+      </select>
+    </form>
+
+    <form id="exampleform2">
+      <input type="text" name="test1">
+      <input type="text">
+      <input type="text" name="arraylike">
+      <input type="text" name="arraylike">
+      <input type="checkbox" name="checkbox">
+      <input type="checkbox" name="checkbox">
+      <input type="radio" name="gender" value="male">
+      <input type="radio" name="gender" value="female">
+      <input type="radio" name="gender" value="other">
+      <select name="color">
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+      </select>
+      <select name="sizes" multiple>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+      </select>
+    </form>
+  `;
+
+  form1 = formObj(document.getElementById('exampleform'));
+  form2 = formObj(document.getElementById('exampleform2'));
+};
+
+init();
+
 test('implementation', assert => {
-  /*
-    These don't really test the class as they're really just to
-    make sure the testing environment is setup correctly.
-  */
-  assert.ok(Array.isArray(window.logHistory), 'window.logHistory setup');
+  setup();
   assert.equal(typeof formObj, 'function', 'FormObj class exists');
 
   assert.end();
 });
 
 test('serialize', assert => {
+  setup();
   assert.equal(form1.getInputs().length, 10, 'Finds inputs');
   assert.deepEqual(form1.getJSON(), {
     test1: 'test',
@@ -39,6 +96,7 @@ test('serialize', assert => {
 });
 
 test('serialize', assert => {
+  setup();
   const json = form1.getJSON();
 
   form2.deserialize(json);
@@ -57,6 +115,21 @@ test('serialize', assert => {
       'medium'
     ]
   }, 'JSON output');
+
+  assert.end();
+});
+
+test('get query string', assert => {
+  setup();
+  const testObject = {
+    name: 'test',
+    email: 'test@test.com',
+    fruits: ['apple', 'orange']
+  };
+  const qs = getQueryString(testObject);
+
+  assert.equal(qs, 'name=test&email=test%40test.com&fruits=apple&fruits=orange', 'it\'s able to parse an object into a qs');
+  assert.equal(form1.getQueryString(), 'test1=test&arraylike=val1&arraylike=val2&checkbox=true&checkbox=false&gender=female&color=green&sizes=small&sizes=medium', 'it\'s able to parse a form into a qs');
 
   assert.end();
 });
