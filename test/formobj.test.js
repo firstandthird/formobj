aaeefbf7fe9830d2194e4ba2cdfeb0ccbc790b33
Antonio Laguna <sombragriselros@gmail.com>
2017-03-02 12:55:30 +0100

    Updating checkbox behaviour

--- test/formobj.test.js
+++ test/formobj.test.js
@@ -6,6 +6,7 @@ import test from 'tape-rollup';
 
 let form1;
 let form2;
+let formCheckbox;
 
 const init = () => {
   const container = document.createElement('div');
@@ -21,8 +22,9 @@ const setup = () => {
       <input type="text" value="nope">
       <input type="text" name="arraylike" value="val1">
       <input type="text" name="arraylike" value="val2">
-      <input type="checkbox" name="checkbox" checked>
-      <input type="checkbox" name="checkbox">
+      <input type="checkbox" name="checkbox" value="val1" checked>
+      <input type="checkbox" name="checkbox" value="val2">
+      <input type="checkbox" name="checked" checked>
       <input type="radio" name="gender" value="male">
       <input type="radio" name="gender" value="female" checked>
       <input type="radio" name="gender" value="other">
@@ -43,8 +45,9 @@ const setup = () => {
       <input type="text">
       <input type="text" name="arraylike">
       <input type="text" name="arraylike">
-      <input type="checkbox" name="checkbox">
-      <input type="checkbox" name="checkbox">
+      <input type="checkbox" name="checkbox" value="val1">
+      <input type="checkbox" name="checkbox" value="val2">
+      <input type="checkbox" name="checked">
       <input type="radio" name="gender" value="male">
       <input type="radio" name="gender" value="female">
       <input type="radio" name="gender" value="other">
@@ -59,10 +62,19 @@ const setup = () => {
         <option value="large">Large</option>
       </select>
     </form>
+    
+    <form id="checkboxform">
+      <input type="checkbox" name="valuecheckbox" value="val1" checked>
+      <input type="checkbox" name="valuecheckbox" value="val2">
+      <input type="checkbox" name="valuecheckbox" value="val3" checked>
+      <input type="checkbox" name="singlevaluecheckbox" value="val1" checked>
+      <input type="checkbox" name="novaluecheckbox" checked>
+    </form>
   `;
 
   form1 = formObj(document.getElementById('exampleform'));
   form2 = formObj(document.getElementById('exampleform2'));
+  formCheckbox = formObj(document.getElementById('checkboxform'));
 };
 
 init();
@@ -76,14 +88,15 @@ test('implementation', assert => {
 
 test('serialize', assert => {
   setup();
-  assert.equal(form1.getInputs().length, 10, 'Finds inputs');
+  assert.equal(form1.getInputs().length, 11, 'Finds inputs');
   assert.deepEqual(form1.getJSON(), {
     test1: 'test',
     arraylike: [
       'val1',
       'val2'
     ],
-    checkbox: [true, false],
+    checkbox: 'val1',
+    checked: true,
     gender: 'female',
     color: 'green',
     sizes: [
@@ -107,7 +120,8 @@ test('serialize', assert => {
       'val1',
       'val2'
     ],
-    checkbox: [true, false],
+    checkbox: 'val1',
+    checked: true,
     gender: 'female',
     color: 'green',
     sizes: [
@@ -129,7 +143,19 @@ test('get query string', assert => {
   const qs = getQueryString(testObject);
 
   assert.equal(qs, 'name=test&email=test%40test.com&fruits=apple&fruits=orange', 'it\'s able to parse an object into a qs');
-  assert.equal(form1.getQueryString(), 'test1=test&arraylike=val1&arraylike=val2&checkbox=true&checkbox=false&gender=female&color=green&sizes=small&sizes=medium', 'it\'s able to parse a form into a qs');
+  assert.equal(form1.getQueryString(), 'test1=test&arraylike=val1&arraylike=val2&checkbox=val1&checked=true&gender=female&color=green&sizes=small&sizes=medium');
+
+  assert.end();
+});
+
+test('checkboxes', assert => {
+  setup();
+  const json = formCheckbox.getJSON();
+
+  assert.deepEqual(
+    json,
+    { novaluecheckbox: true, singlevaluecheckbox: 'val1', valuecheckbox: ['val1', 'val3'] },
+    'Able to understand several types of checkboxes');
 
   assert.end();
 });
